416536e54d7b46d3a90c9171c92b6b0591be21df
Buzinaev Temir <heavy421@gmail.com>
2020-09-14 14:07:31 +0900

    fitst commit

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vvedite pervoe chislo:");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Vvedite vtoroe chislo:");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Vvedite tretie chislo:");
+            int c = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Srednie arefmeticheskoe:");
+            double sa = (a + b + c) / 3;
+            Console.WriteLine(sa);
+            Console.WriteLine("... :");
+            int r = (2 * (a + c)) - (3 * b);
+            Console.WriteLine(r);
+            Console.ReadKey();
+        }
+    }
+}
